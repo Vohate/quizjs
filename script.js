fbc5ace545de26a2810ae418f31b8adb70fe358f
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-01-27 15:44:21 +0100

    Fonction compteur fini

--- script.js
+++ script.js
@@ -31,103 +31,144 @@ const response8 = document.querySelector(".box8 > p");
 const response9 = document.querySelector(".box9 > p");
 const response10 = document.querySelector(".box10 > p");
 
+const showScore = document.getElementById("Note");
+const divScore = document.querySelector(".Score")
+let note = 0;
+let questionResponse = 0;
+let totalQuestions = 10;
 
 btn1.addEventListener("click", () => {
     response1.classList.add("show-response");
-    response1.style.background = "red";
+    response1.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn2.addEventListener("click", () => {
     response1.classList.add("show-response");
-    response1.style.background = "green";
+    response1.style.background = "red";
+    questionResponse++;
 })
 
 btn3.addEventListener("click", () => {
     response2.classList.add("show-response");
     response2.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn4.addEventListener("click", () => {
     response2.classList.add("show-response");
     response2.style.background = "red";
+    questionResponse++;
 })
 
 btn5.addEventListener("click", () => {
     response3.classList.add("show-response");
     response3.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn6.addEventListener("click", () => {
     response3.classList.add("show-response");
     response3.style.background = "red";
+    questionResponse++;
 })
 
 btn7.addEventListener("click", () => {
     response4.classList.add("show-response");
     response4.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn8.addEventListener("click", () => {
     response4.classList.add("show-response");
     response4.style.background = "red";
+    questionResponse++;
 })
 
 btn9.addEventListener("click", () => {
     response5.classList.add("show-response");
     response5.style.background = "red";
+    questionResponse++;
 })
 
 btn10.addEventListener("click", () => {
     response5.classList.add("show-response");
     response5.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn11.addEventListener("click", () => {
     response6.classList.add("show-response");
     response6.style.background = "red";
+    questionResponse++;
 })
 
 btn12.addEventListener("click", () => {
     response6.classList.add("show-response");
     response6.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn13.addEventListener("click", () => {
     response7.classList.add("show-response");
     response7.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn14.addEventListener("click", () => {
     response7.classList.add("show-response");
     response7.style.background = "red";
+    questionResponse++;
 })
 
 btn15.addEventListener("click", () => {
     response8.classList.add("show-response");
     response8.style.background = "red";
+    questionResponse++;
 })
 
 btn16.addEventListener("click", () => {
     response8.classList.add("show-response");
     response8.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn17.addEventListener("click", () => {
     response9.classList.add("show-response");
     response9.style.background = "green";
+    note++;
+    questionResponse++;
 })
 
 btn18.addEventListener("click", () => {
     response9.classList.add("show-response");
     response9.style.background = "red";
+    questionResponse++;
 })
 
 btn19.addEventListener("click", () => {
     response10.classList.add("show-response");
     response10.style.background = "green";
+    note++;
+    questionResponse++;
+    divScore.classList.add("showScore");
+    showScore.innerHTML = `${note}`;
 })
 
 btn20.addEventListener("click", () => {
     response10.classList.add("show-response");
     response10.style.background = "red";
+    questionResponse++;
+    divScore.classList.add("showScore");
+    showScore.innerHTML = `${note}`;
 })
+
+
